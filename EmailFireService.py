cbab7265f14dca912e9551438c86f5f0e1dc3ae3
Bright Okyere <57848447+BrightJ728@users.noreply.github.com>
2022-05-12 21:34:06 +0000

    Update EmailFireService.py

--- EmailFireService.py
+++ EmailFireService.py
@@ -1,17 +1,19 @@
 import smtplib, ssl
+def sendEmail():
+      port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "finalcapstone360@gmail.com"  # Enter your address
+    receiver_email = "domfehadwoamary@gmail.com"  # Enter receiver address
+    password = "marybright"
+    message = """\
+    Subject: FireSavers- FireAlert 
 
-port = 465  # For SSL
-smtp_server = "smtp.gmail.com"
-sender_email = "finalcapstone360@gmail.com"  # Enter your address
-receiver_email = "domfehadwoamary@gmail.com"  # Enter receiver address
-password = "marybright"
-message = """\
-Subject: FireSavers- FireAlert 
-
-This message is to notify of a fire alert. Kindly check the dashboard
-for Location  details."""
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    This message is to notify of a fire alert. Kindly check the dashboard
+    for Location  details."""
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+       
+if __name__=="__main__":
+    sendEmail()
