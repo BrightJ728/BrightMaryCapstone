7539a79cde379a7b960c7eb9eca8762cc587951e
Bright Okyere <57848447+BrightJ728@users.noreply.github.com>
2022-05-12 21:40:01 +0000

    Update SMSTwilio.py

--- SMSTwilio.py
+++ SMSTwilio.py
@@ -1,15 +1,18 @@
 from twilio.rest import Client
 
-# Your Account SID from twilio.com/console
-account_sid = "AC90951957f5eb34584c634ada2bce6ebc"
-# Your Auth Token from twilio.com/console
-auth_token  = "f0b9d48bdd3e16eb0bc00b87696db2f6"
+def Twilosms():
+    # Your Account SID from twilio.com/console
+    account_sid = "AC90951957f5eb34584c634ada2bce6ebc"
+    # Your Auth Token from twilio.com/console
+    auth_token  = "f0b9d48bdd3e16eb0bc00b87696db2f6"
 
-client = Client(account_sid, auth_token)
+    client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to="+233248697356", 
-    from_="+19704788792",
-    body="SMS with Twilio - Fire alert!")
+    message = client.messages.create(
+        to="+233248697356", 
+        from_="+19704788792",
+        body="SMS with Twilio - Fire alert!")
 
-print(message.sid)
+    print(message.sid)
+if __name__=="__main__":
+    Twilosms()
